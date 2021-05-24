82e0d46f533e90b36015c5c71c98952ac9a3146f
Shikha Triapthi <tripathi.reshu22@gmail.com>
2021-05-25 02:16:09 +0530

    blank commit

--- match-info-service/src/main/java/com/example/matchinfo/MatchInfoApplication.java
+++ match-info-service/src/main/java/com/example/matchinfo/MatchInfoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MatchInfoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MatchInfoApplication.class, args);
 	}
 
